7e2d285765ac944a8e7d9913f263d078e0f3c11c
Jared Husson <jh1528@.g.rit.edu>
2026-06-05 12:21:12 -0400

    Add Activity 3 Terraform Azure VM setup
    Add Terraform configuration files for the lab 3 Activity 3 Azure VM deployment.  This commit includes .gitignore, main.tf, providers.tf, terraform.tfvars, and variables.tf.
    
    The deployment is still being tested because the default eastus StandardB1s VM is restricted for the student subscription.

---
+++ providers.tf
@@ -0,0 +1,18 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "11c145bc-4a8c-47cb-aeee-31536d2331c0"
+}
